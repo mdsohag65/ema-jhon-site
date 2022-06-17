df5e22d4d3447e9248531b6e042b36c2576e6b37
mdsohag65 <sohag92ahmed@gmail.com>
2022-06-17 12:26:44 +0600

    done the navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Order from './components/Order/Order';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='order' element={<Order />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Order/Order.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Order = () => {
+    return (
+        <div>
+            <h1>This is Order section</h1>
+        </div>
+    );
+};
+
+export default Order;
