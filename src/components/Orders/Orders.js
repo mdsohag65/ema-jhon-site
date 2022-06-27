b8dd7dc7450b49e8aef48dd9d6409a7155918a31
mdsohag65 <sohag92ahmed@gmail.com>
2022-06-27 18:23:28 +0600

    make another custom Hook

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
 
 const Orders = () => {
     const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products);
     return (
         <div>
             <h2 className='text-2xl text-center font-bold'>Your Orders: {products.length}</h2>
+            <p className='text-center font-bold'>Cart has: {cart.length}</p>
         </div>
     );
 };
